28c13c63222866bc5745e907ce81bc68c0ce36be
Ryan Barner <rbarner14@gmail.com>
2023-04-09 00:08:25 -0700

    Update
    Signed-off-by: Ryan Barner <rbarner14@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = props => {
   const cartitems = <ul className={classes['cart-items']}>{[
@@ -11,7 +12,7 @@ const Cart = props => {
   ].map(item => <li>{item.name}</li>)}</ul>;
 
   return (
-    <div>
+    <Modal>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -21,7 +22,7 @@ const Cart = props => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 };
 
