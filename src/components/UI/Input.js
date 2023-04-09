6291a2e872045dc915c6bd296c66bed8c314983b
Ryan Barner <rbarner14@gmail.com>
2023-04-08 23:36:35 -0700

    UNIQUE
    Signed-off-by: Ryan Barner <rbarner14@gmail.com>

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,17 +1,12 @@
 import classes from "./Input.module.css";
 
 const Input = props => {
-  return <div className={classes.input}>
-    <label htmlFor={props.input.id}>{props.label}</label>
-    {/*spread operator is a trick one can use to pass all other config data*/}
-    {/*to input element as props; all key value pairs in the input object*/}
-    {/*are added as props to input*/}
-    {/*ie {type: "text"}*/}
-    {/*would make*/}
-    {/*<input type="text" />*/}
-    {/*makes element highly configurable from outside the component through a prop object*/}
-    <input {...props.input} />
-  </div>
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  )
 };
 
 export default Input;
