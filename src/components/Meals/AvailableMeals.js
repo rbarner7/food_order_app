57e3464f42db57af3362c5646d9b1de8f92a88f5
Ryan Barner <rbarner14@gmail.com>
2023-04-08 22:20:59 -0700

    Input component
    Signed-off-by: Ryan Barner <rbarner14@gmail.com>

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -30,7 +30,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,21 @@
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = props => {
+  return (
+    <form className={classes.form}>
+      {/*input object argument is default props that can be added to any element*/}
+      <Input label="Amount" input={{
+        id: 'amount',
+        type: 'number',
+        min: '1',
+        max: '5',
+        step: '1',
+        defaultValue: '1',
+      }}/>
+      <button>+ Add</button>
+    </form>
+  )
+};
+
+export default MealItemForm;
